89b5ea88461ce3f2c7167f36980de05b585a297f
Wietze <wietze@users.noreply.github.com>
2017-09-10 18:44:45 +0100

    Solution to challenge 43 (set 6)

--- set6.py
+++ set6.py
@@ -113,7 +113,83 @@ def challenge_42():
     # Check that the forged message passes verification
     assert_true(m.verify(forged_message, fake_signed_message))
 
+
+## Challenge 43
+class DSA:
+    # Based on https://en.wikipedia.org/wiki/Digital_Signature_Algorithm
+    def __init__(self, p, q, g):
+        self.p, self.q, self.g = p, q, g
+
+    def __get_hash__(self, msg):
+        return int(hashlib.sha1(msg).hexdigest(), 16)
+
+    def generate_keypair(self):
+        x = random.randrange(0, self.q)
+        p = set5.modexp(self.g, x, self.p)
+        return (x, p)
+
+    def sign(self, msg, x):
+        k = random.randrange(2, self.q)
+        r = set5.modexp(self.g, k, self.p) % self.q
+        if k == 0: return self.sign(msg, x)
+        s = (set5.modinv(k, self.q) * (self.__get_hash__(msg) + (x*r))) % self.q
+        if s == 0: return self.sign(msg, x)
+        return (r, s), k
+
+    def verify(self, msg, sig, y):
+        r, s = sig
+        if not(0 < r and r < self.q): raise ValueError('Ir not between 0 and q (r={})'.format(r))
+        if not(0 < s and s < self.q): raise ValueError('s not between 0 and q (s={})'.format(s))
+        w = set5.modinv(s, self.q)
+        u1 = (self.__get_hash__(msg) * w) % self.q
+        u2 = (r * w) % self.q
+        v = ((set5.modexp(self.g, u1, self.p) * set5.modexp(y, u2, self.p)) % self.p) % self.q
+        return v == r
+
+    def crack_x(self, k, msg, sig):
+        r, s = sig
+        return (((s * k) - self.__get_hash__(msg)) * set5.modinv(r, self.q)) % self.q
+
+@challenge(6, 43)
+def challenge_43():
+    # Set parameters p, q, g
+    p = 0x800000000000000089e1855218a0e7dac38136ffafa72eda7859f2171e25e65eac698c1702578b07dc2a1076da241c76c62d374d8389ea5aeffd3226a0530cc565f3bf6b50929139ebeac04f48c3c84afb796d61e5a4f9a8fda812ab59494232c7d2b4deb50aa18ee9e132bfa85ac4374d7f9091abc3d015efc871a584471bb1
+    q = 0xf4f47f05794b256174bba6e9b396a7707e563c5b
+    g = 0x5958c9d3898b224b12672c0b98e06c60df923cb8bc999d119458fef538b8fa4046c8db53039db620c094c9fa077ef389b5322a559946a71903f990f1f7e0e025e2d7f7cf494aff1a0470f5b64c36b625a097f1651fe775323556fe00b3608c887892878480e99041be601a62166ca6894bdd41a7054ec89f756ba9fc95302291
+
+    message = b'Who is the king of the jungle?'
+
+    # Set up new DSA instance and keypair
+    dsa = DSA(p, q, g)
+    x,y = dsa.generate_keypair()
+
+    # Part 1: try to verify a valid signature
+    sig, k = dsa.sign(message, x)
+    assert dsa.verify(message, sig, y)
+
+    # Part 2: try and obtain x
+    assert dsa.crack_x(k, message, sig) == x
+
+    # Part 3: try and brute force private key based on signature and message#
+    message = b'For those that envy a MC it can be hazardous to your health\nSo be friendly, a matter of life and death, just like a etch-a-sketch\n'
+    sig = 548099063082341131477253921760299949438196259240, 857042759984254168557880549501802188789837994940
+
+    # Try all `k`s in given range
+    for k in range(0, 2 ** 16):
+        # Try k, get private key `x`
+        x = dsa.crack_x(k, message, sig)
+        # Verify hash of found private key `x`
+        if hashlib.sha1(hex(x)[2:].encode()).hexdigest() == '0954edd5e0afe5542a4adf012611a91912a3ec16':
+            print("The private key is x={}".format(x))
+            assert_true(True)
+            break
+    else:
+        print('Unable to find x')
+        assert_true(False)
+
+
 ## Execute individual challenges
 if __name__ == '__main__':
     challenge_41()
     challenge_42()
+    challenge_43()
